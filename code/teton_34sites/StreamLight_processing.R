9f5bfe5d5b81f7b39658a4d9108af1f96e0203f1
Heili Lowman <heili.lowman@gmail.com>
2021-07-13 10:47:05 -0700

    Trimmed and exported data for 17 of 34 sites that have StreamLight (Savoy) data.

--- code/teton_34sites/StreamLight_processing.R
+++ code/teton_34sites/StreamLight_processing.R
@@ -1,10 +1,10 @@
 ## StreamLight Output Script
-## June 23, 2021
+## July 13, 2021
 ## Heili Lowman
 
-# I'll be modifying some of Joanna's code from the RiverBiomass repository
-# to practice fitting the Ricker model to 1 year of data at a "good" and
-# "bad" site.
+# I'll be modifying Joanna's code from the RiverBiomass repository
+# to practice fitting the Ricker model to ~30 site-years of data.
+# The compiled data will then be run by sending the job to Teton (UWyoming).
 
 # I've commented out those steps that I feel, for the time being, I don't
 # need to perform, and I've changed the appropriate filepaths to match my
@@ -13,16 +13,23 @@
 lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
          "tidyverse","data.table","patchwork", "here"), require, character.only=T)
 
+# Load in datasets created in NWIS_RiverSelection script.
+NWIS_34sites_subset <- readRDS("data_working/NWIS_34sites_subset.rds")
+NWIS_34sitesinfo_subset <- readRDS("data_working/NWIS_34sitesinfo_subset.rds")
+NWIS_34sites_Ndays <- readRDS("data_working/NWIS_34sites_Ndays.rds")
+
 ## Site of interest that has StreamLight data
-sites <- c("nwis_01608500")
+sites <- NWIS_34sitesinfo_subset$site_name
 sites_files <- rep(NA, length(sites))
 for(i in 1:length(sites)){
   sites_files[i] <- paste(sites[i],"_input_output.txt", sep="")
 }
 
-## Import streamlight
+# need to identify the intersection of files with available light data
+filenames <- intersect(sites_files, list.files(here("data_raw", "individual_files")))
 
-SL <- ldply(sites_files, function(filename) {
+## Import StreamLight
+SL <- ldply(filenames, function(filename) {
   d <- read.table(here("data_raw", "individual_files", filename), header = T, sep = "\t")
   d$file <- filename
   return(d)
@@ -30,7 +37,6 @@ SL <- ldply(sites_files, function(filename) {
 
 ## take the mean daily incoming PAR at the surface
 SL_split <- split(SL, SL$file)
-View(SL_split$nwis_01608500_input_output.txt)
 
 meandaily_PAR <- function(y){
   df <- y %>%
@@ -44,14 +50,14 @@ meandaily_PAR <- function(y){
 }
 
 SL_daily <- lapply(SL_split, function(x) meandaily_PAR(x))
-tail(SL_daily$nwis_01608500_input_output.txt)
 
 ## visualize
-ggplot(SL_daily$nwis_01608500_input_output.txt, aes(Date, PAR_surface))+
+ggplot(SL_daily$nwis_14206950_input_output.txt, aes(Date, PAR_surface))+
   geom_point()+
-  geom_point(aes(Date, PAR_turb), color="blue")+
-  labs(title = "South Branch Potomac River, WV")
-
+  geom_point(aes(Date, PAR_turb), color="blue") +
+  labs(x = "Date",
+       y = "Daily PAR",
+       title = "Fanno Creek - Durham, OR")
 
 ###########################
 ## Subset and evaluate NA
@@ -61,16 +67,16 @@ SF_df <- ldply(SL_daily, data.frame)
 ## add site_name
 SF_df$site_name <- substr(SF_df$.id, 1, nchar(SF_df$.id)-17)
 
-## From NWIS_RiverSelection
-site_subset <- rbind(SF_df[which(SF_df$site_name == "nwis_01608500" & SF_df$Year %in% c(2015)),])
+site_subset <- SF_df
 
 site_subset_split <- split(site_subset, site_subset$.id)
 
-lapply(site_subset_split, function(x) sum(is.na(x$PAR_surface))) # all 0
-lapply(site_subset_split, function(x) sum(is.na(x$PAR_turb))) # 60
+# what are these doing??
+lapply(site_subset_split, function(x) sum(is.na(x$PAR_surface)))
+lapply(site_subset_split, function(x) sum(is.na(x$PAR_turb)))
 
 ## Save
 
-saveRDS(site_subset, "data_working/StreamLight_daily_1riv_good_2015.rds")
+saveRDS(site_subset, "data_working/StreamLight_daily_34sites.rds")
 
 # End of script.
