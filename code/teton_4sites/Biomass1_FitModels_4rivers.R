4065b2b7bb83ada182921c1d638a4c14403bfb0a
Heili Lowman <heili.lowman@gmail.com>
2022-01-20 15:40:56 -0800

    Edited Biomass script to create new function to delineate between which script to use.

---
+++ code/teton_4sites/Biomass1_FitModels_4rivers.R
@@ -0,0 +1,92 @@
+## Fitting models to data
+## Step THREE in Metabolism Modeling Workflow
+## January 20, 2022
+## Heili Lowman
+
+# I'll be modifying Joanna's code from the RiverBiomass repository
+# to fit the Ricker model to 4 sites of data.
+
+# load packages
+lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
+         "parallel","tidyverse","rstan","devtools",
+         "bayesplot","shinystan","here"), 
+       require, character.only=T)
+
+#### Run 1 ####
+
+## Source data
+df <- readRDS("/project/modelscape/users/hlowman/jobscripts/teton_4sites/df_4sites.rds")
+df <- readRDS("data_working/df_4sites.rds")
+
+# for test, pull out only sites not requiring P
+test <- df[c(1,2)]
+
+####################
+## Stan data prep ##
+####################
+rstan_options(auto_write=TRUE)
+## specify number of cores
+options(mc.cores=6)
+
+## compile data
+stan_data_compile <- function(x){
+  data <- list(Ndays=length(x$GPP), 
+               light = x$light_rel, 
+               GPP = x$GPP,
+               GPP_sd = x$GPP_sd, 
+               tQ = x$tQ,
+               new_e = x$new_e, # new column for reinitialization of B[j] values
+               p_remove = x$p_remove[1]) # new column for model selection (with/without P)
+  return(data)
+}
+
+stan_data_l <- lapply(df, function(x) stan_data_compile(x))
+stan_data_test <- lapply(test, function(x) stan_data_compile(x))
+
+#########################################
+## Run Stan to get parameter estimates - all sites
+#########################################
+
+# Latent Biomass (Ricker population) Model
+
+# sets initial values of c and s to help chains converge
+init_Ricker <- function(...) {
+  list(c = 0.5, s = 100)
+}
+
+# create stan function that delineates between two scripts to use
+p_choose <- function(dat){
+  if(dat$p_remove == 1){
+    return("code/teton_4sites/Stan_ProductivityModel2_Ricker_fixedinit_obserr_ts_noP.stan")
+  } else {
+    return("code/teton_4sites/Stan_ProductivityModel2_Ricker_fixedinit_obserr_ts_wP.stan")
+  }
+}
+
+p_choose(stan_data_test$nwis_02266300)
+
+## export results
+PM_outputlist_Ricker <- lapply(stan_data_test,
+                               
+                               # p = 1, so remove P
+                               
+                                 # function(x) stan("/project/modelscape/users/hlowman/jobscripts/teton_4sites/Stan_ProductivityModel2_Ricker_fixedinit_obserr_ts_noP.stan",
+                                 function(x) stan(p_choose(x),
+                                   data = x,chains = 3,iter = 5000,
+                                   init = init_Ricker,
+                                   control = list(max_treedepth = 12)) )
+                               } 
+                               
+                               # p = 0, so keep P
+                               else {
+                                 # function(x) stan("/project/modelscape/users/hlowman/jobscripts/teton_4sites/Stan_ProductivityModel2_Ricker_fixedinit_obserr_ts_wP.stan",
+                                 function(x) stan("code/teton_4sites/Stan_ProductivityModel2_Ricker_fixedinit_obserr_ts_wP.stan",
+                                   data = x,chains = 3,iter = 5000,
+                                   init = init_Ricker,
+                                   control = list(max_treedepth = 12))
+                               }
+                               )
+
+saveRDS(PM_outputlist_Ricker, "/project/modelscape/users/hlowman/jobresults/teton_4sites/teton_4rivers_output_Ricker_2022_01_22.rds")
+
+# End of script.
