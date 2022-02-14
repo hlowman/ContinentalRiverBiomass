6b3f14d6ab31b591326a8dca0b838acf88eb3ad2
Heili Lowman <heili.lowman@gmail.com>
2022-02-14 10:51:39 -0800

    Added code for predicting GPP to supplementary figure script depicting the purpose of removing the P term.

--- code/supp_figures/Biomass1_FitModel.R
+++ code/supp_figures/Biomass1_FitModel.R
@@ -101,6 +101,45 @@ data_out_params_noP <- map(PM_outputlist_Ricker_noP, extract_params)
 params_noP_df <- map_df(data_out_params_noP, ~as.data.frame(.x), .id="site_name") %>%
   mutate(model = "without P")
 
+#### GPP Predictions ####
+
+# Using code from Joanna's script "Predicted_ProductivityModel_Ricker.R".
+
+## Growth Model 3 - Data simulation
+
+PM_Ricker <- function(r, lambda, s, c, sig_p, sig_o, df) {
+  
+  ## Data
+  Ndays<-length(df$GPP)
+  GPP <- df$GPP
+  GPP_sd <- df$GPP_sd
+  light <- df$light_rel
+  tQ <- df$tQ # discharge standardized to max value
+  
+  ## Vectors for model output of P, B, pred_GPP
+  P <- numeric(Ndays)
+  P[1] <- 1
+  for(i in 2:length(tQ)){
+    P[i] = exp(-exp(s*(tQ[i] - c)))
+  }
+  
+  B<-numeric(Ndays)
+  B[1] <- log(GPP[1]/light[1])
+  pred_GPP<-numeric(Ndays)
+  pred_GPP[1] <- light[1]*exp(B[1])
+  
+  ## Process Model
+  for (j in 2:Ndays){
+    B[j] <- MCMCglmm::rtnorm(1, mean = (B[j-1] + r + lambda*exp(B[j-1]))*P[j], sd = sig_p, upper = 5)
+  }
+  
+  for (i in 2:Ndays){
+    pred_GPP[i] <- MCMCglmm::rtnorm(1, mean = light[i]*exp(B[i]), sd = sig_o, lower=0.01)
+  }
+  
+  return(pred_GPP)
+}
+
 # NOTE TO FUTURE SELF - need to add intervals to this figure
 
 rivers_gpp <- rivers %>%
