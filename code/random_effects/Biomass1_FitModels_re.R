1ca276e6df61eb6e7eb32930a78425d79944dc3e
Heili Lowman <heili.lowman@gmail.com>
2022-04-01 15:39:44 -0700

    Created matrices for light, GPP, Q, and reinitialization indexing data inputs.

--- code/random_effects/Biomass1_FitModels_re.R
+++ code/random_effects/Biomass1_FitModels_re.R
@@ -15,6 +15,8 @@ lapply(c("plyr","dplyr","ggplot2","cowplot","lubridate",
 ## Source data - with new index number by site
 df <- readRDS("data_working/df_207sites_indexed.rds")
 
+#### Creating Necessary Matrices ####
+
 # Testing how to pull data from the assembled list into a matrix to pass to the stan file
 # Pull out only light_rel column from each dataframe within the list
 names <- "light_rel"
@@ -53,6 +55,146 @@ test_light_mx <- matrix(unlist(test_light), nrow = 3208, ncol = 207)
 # 3208 days is the longest time series
 # Yippee!! This works!!
 
+# Renaming matrix for use in model test below.
+light_mx <- test_light_mx
+
+# Repeat for GPP
+# Pull out only GPP column from each dataframe within the list
+names2 <- "GPP"
+subset_gpp <- lapply(df, "[", names2)
+
+# Count the length of each line
+length_df2 <- function(x){
+  data <- length(x$GPP) # need to go two layers in to calculate length of GPP column
+  return(data)
+}
+
+line_lengths2 <- lapply(subset_gpp, length_df2) # apply function to full list
+
+line_lengths2 <- t(as.data.frame(line_lengths2)) # and transpose into a dataframe
+
+# Pad shorter lines with NA below
+subset_gpp[which(line_lengths2 != max(line_lengths2))] <- 
+  lapply(subset_gpp[which(line_lengths2 != max(line_lengths2))], function(x){
+    # create list of existing GPP values
+    list1 <- x$GPP
+    list1 <- as.data.frame(list1) %>%
+      rename(GPP = list1) 
+    # create list of NAs to be added
+    list2 <- rep(NA, times = max(line_lengths2)-length(x$GPP))
+    list2 <- as.data.frame(list2) %>%
+      rename(GPP = list2)
+    # join the two together
+    rbind(list1, list2)
+  })
+
+# Use the list created above to create a matrix of GPP values
+gpp_mx <- matrix(NA, 3208, 207)
+gpp_mx <- matrix(unlist(subset_gpp), nrow = 3208, ncol = 207)
+# WOOHOO
+
+# And for sd of GPP.
+# Pull out only GPP_sd column from each dataframe within the list
+names3 <- "GPP_sd"
+subset_gppsd <- lapply(df, "[", names3)
+
+# Count the length of each line
+length_df3 <- function(x){
+  data <- length(x$GPP_sd) # need to go two layers in to calculate length of GPP_sd column
+  return(data)
+}
+
+line_lengths3 <- lapply(subset_gppsd, length_df3) # apply function to full list
+
+line_lengths3 <- t(as.data.frame(line_lengths3)) # and transpose into a dataframe
+
+# Pad shorter lines with NA below
+subset_gppsd[which(line_lengths3 != max(line_lengths3))] <- 
+  lapply(subset_gppsd[which(line_lengths3 != max(line_lengths3))], function(x){
+    # create list of existing GPP sd values
+    list1 <- x$GPP_sd
+    list1 <- as.data.frame(list1) %>%
+      rename(GPP_sd = list1) 
+    # create list of NAs to be added
+    list2 <- rep(NA, times = max(line_lengths3)-length(x$GPP_sd))
+    list2 <- as.data.frame(list2) %>%
+      rename(GPP_sd = list2)
+    # join the two together
+    rbind(list1, list2)
+  })
+
+# Use the list created above to create a matrix of GPP values
+gpp_sd_mx <- matrix(NA, 3208, 207)
+gpp_sd_mx <- matrix(unlist(subset_gppsd), nrow = 3208, ncol = 207)
+# WOOT
+
+# And for discharge.
+# Pull out only tQ column from each dataframe within the list
+names4 <- "tQ"
+subset_tQ <- lapply(df, "[", names4)
+
+# Count the length of each line
+length_df4 <- function(x){
+  data <- length(x$tQ) # need to go two layers in to calculate length of tQ column
+  return(data)
+}
+
+line_lengths4 <- lapply(subset_tQ, length_df4) # apply function to full list
+
+line_lengths4 <- t(as.data.frame(line_lengths4)) # and transpose into a dataframe
+
+# Pad shorter lines with NA below
+subset_tQ[which(line_lengths4 != max(line_lengths4))] <- 
+  lapply(subset_tQ[which(line_lengths4 != max(line_lengths4))], function(x){
+    # create list of existing tQ values
+    list1 <- x$tQ
+    list1 <- as.data.frame(list1) %>%
+      rename(tQ = list1) 
+    # create list of NAs to be added
+    list2 <- rep(NA, times = max(line_lengths4)-length(x$tQ))
+    list2 <- as.data.frame(list2) %>%
+      rename(tQ = list2)
+    # join the two together
+    rbind(list1, list2)
+  })
+
+# Use the list created above to create a matrix of tQ values
+tQ_mx <- matrix(NA, 3208, 207)
+tQ_mx <- matrix(unlist(subset_tQ), nrow = 3208, ncol = 207)
+
+# and finally the reinitialization index values.
+# Pull out only new_e column from each dataframe within the list
+names5 <- "new_e"
+subset_e <- lapply(df, "[", names5)
+
+# Count the length of each line
+length_df5 <- function(x){
+  data <- length(x$new_e) # need to go two layers in to calculate length of new_e column
+  return(data)
+}
+
+line_lengths5 <- lapply(subset_e, length_df5) # apply function to full list
+
+line_lengths5 <- t(as.data.frame(line_lengths5)) # and transpose into a dataframe
+
+# Pad shorter lines with NA below
+subset_e[which(line_lengths5 != max(line_lengths5))] <- 
+  lapply(subset_e[which(line_lengths5 != max(line_lengths5))], function(x){
+    # create list of existing e values
+    list1 <- x$new_e
+    list1 <- as.data.frame(list1) %>%
+      rename(new_e = list1) 
+    # create list of NAs to be added
+    list2 <- rep(NA, times = max(line_lengths5)-length(x$new_e))
+    list2 <- as.data.frame(list2) %>%
+      rename(new_e = list2)
+    # join the two together
+    rbind(list1, list2)
+  })
+
+# Use the list created above to create a matrix of tQ values
+e_mx <- matrix(NA, 3208, 207)
+e_mx <- matrix(unlist(subset_e), nrow = 3208, ncol = 207)
 
 ####################
 ## Stan data prep ##
