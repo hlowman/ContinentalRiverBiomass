e0338d1084191e8aac8fa8aaa3fa5b906e9743e9
Heili Lowman <heili.lowman@gmail.com>
2022-07-07 13:34:28 -0700

    Added code to send two more test models to Teton.

--- code/teton_summer22/StreamLight_data_assembly.R
+++ code/teton_summer22/StreamLight_data_assembly.R
@@ -40,6 +40,50 @@ myfilter <- function(x){
 
 filterSL <- lapply(rawSL, myfilter) # only 7 sites matched :(
 
+# Ok, so looks like I'll need to work back through the StreamLight workflow.
+
+# Just kidding! Sometimes an extra 0 is added to site names, so workflow below should take care of that and re-match the dataframes.
+
+# Using Phil's code to pull the sites I need
+sites_100 <- siteswoSL
+
+#Merge together the site information
+info_merged <- merge(
+  sites_100[, c("nwis_id", "nhdplus_id", "lat", "lon", "long_name")], 
+  lotic_site_info_full, 
+  by.x = "long_name",
+  by.y = "Name"
+)
+
+#Subset the list just for the sites of interest
+standard_ized_subset <- lotic_standardized_full[info_merged[, "Site_ID"]]
+
+#Check to see if there are no predictions of light at the stream surface
+light_check <- do.call(
+  rbind,
+  lapply(
+    standard_ized_subset,
+    FUN = function(site){all(is.na(site[, "Stream_PAR_sum"]))}
+  )  
+) %>%
+  as.data.frame()
+
+light_check$Site_ID <- rownames(light_check)
+names(light_check)[1] <- "no_light"
+
+#Get string of sites that do have light estimates
+has_light <- light_check %>%
+  filter(no_light == FALSE) %>%
+  pull(Site_ID)
+
+# End of Phil's code.
+
+# Subset the list just for the sites with stream light data
+final_subset <- standard_ized_subset[has_light]
+
+# Export data
+saveRDS(final_subset, "data_working/StreamLight_daily_85site_list.rds")
+
 # End of script.
 
 
