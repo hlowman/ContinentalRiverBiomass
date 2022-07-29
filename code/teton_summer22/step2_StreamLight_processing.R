c069e0984f4761b7c2ad13c58dc67d8b6e17d9b1
Heili Lowman <heili.lowman@gmail.com>
2022-07-29 06:59:00 -0700

    Joined available stream light data from Allison's dataset (n = 107) with stream light data provided by Phil for his work on the Bernhardt paper (n = 85).

--- code/teton_summer22/step2_StreamLight_processing.R
+++ code/teton_summer22/step2_StreamLight_processing.R
@@ -9,9 +9,10 @@
 # found at: https://github.com/jrblaszczak/RiverBiomass 
 
 # Please note, the "data_raw" and "data_working" folders have been ignored
-# using git.ignore, so links to the raw data sets are provided below.
-# If you are accessing the code via GitHub, these will need to be downloaded 
-# and added to a folder of the appropriate name prior to running the code.
+# using git.ignore, so links to the raw data sets are provided in the step1
+# file. If you are accessing the code via GitHub, these will need to be 
+# downloaded and added to a folder of the appropriate name prior to running the 
+# code.
 
 ############################
 ## Setup
@@ -21,9 +22,9 @@ lapply(c("tidyverse", "plyr", "cowplot","lubridate",
          "data.table","patchwork", "here"), require, character.only=T)
 
 # Load in datasets created in NWIS_RiverSelection script.
-NWIS_207sites_subset <- readRDS("data_working/NWIS_207sites_subset.rds")
-NWIS_207sitesinfo_subset <- readRDS("data_working/NWIS_207sitesinfo_subset.rds")
-NWIS_207sites_Ndays <- readRDS("data_working/NWIS_207sites_Ndays.rds")
+NWIS_207sites_subset <- readRDS("data_working/NWIS_207sites_subset2022.rds")
+NWIS_207sitesinfo_subset <- readRDS("data_working/NWIS_207sitesinfo_subset2022.rds")
+NWIS_207sites_Ndays <- readRDS("data_working/NWIS_207sites_Ndays2022.rds")
 
 # Load in dataset created in StreamLight_data_assembly script.
 SL_85sites <- readRDS("data_working/StreamLight_daily_85site_list.rds")
@@ -66,23 +67,44 @@ meandaily_PAR <- function(y){
 SL_daily <- lapply(SL_split, function(x) meandaily_PAR(x))
 
 ###########################
-## Subset and evaluate NA
+## Join and evaluate NAs
 ###########################
-SF_df <- ldply(SL_daily, data.frame)
+# Appling SL data
+SL_App_df <- ldply(SL_daily, data.frame)
 
 ## add site_name
-SF_df$site_name <- substr(SF_df$.id, 1, nchar(SF_df$.id)-17)
+SL_App_df$site_name <- substr(SL_App_df$.id, 1, nchar(SL_App_df$.id)-17)
 
-site_subset <- SF_df
+# trim down to necessary columns
+SL_App_df <- SL_App_df %>%
+  select(site_name, Date, DOY, Year, PAR_surface)
 
-site_subset_split <- split(site_subset, site_subset$.id)
 
-# Check for NA values.
-lapply(site_subset_split, function(x) sum(is.na(x$PAR_surface)))
-lapply(site_subset_split, function(x) sum(is.na(x$PAR_turb)))
+# Savoy SL data
+SL_Sav_df <- ldply(SL_85sites, data.frame)
 
-## Save
+# trim down to necessary columns
+SL_Sav_df <- SL_Sav_df %>%
+  select(Site_ID, Date, DOY, Year, Stream_PAR_sum) 
 
-saveRDS(site_subset, "data_working/StreamLight_daily_207sites.rds")
+# and rename columns to match the trimmed Appling dataset
+SL_Sav_df <- SL_Sav_df %>%
+  dplyr::rename(site_name = Site_ID,
+         PAR_surface = Stream_PAR_sum)
+
+# And finally JOIN the two together.
+SL_joined_df <- full_join(SL_App_df, SL_Sav_df)
+
+# And create a list form as well.
+SL_joined_list <- split(SL_joined_df, SL_joined_df$site_name)
+
+# Check for NA and NaN values.
+lapply(SL_joined_list, function(x) sum(is.na(x$PAR_surface))) # max ~30/site, most ~0-1/site
+lapply(SL_joined_list, function(x) sum(is.nan(x$PAR_surface))) # max ~30/site, most ~0-1/site
+
+## Save both formats, df and list.
+
+saveRDS(SL_joined_df, "data_working/StreamLight_daily_df_192sites.rds")
+saveRDS(SL_joined_list, "data_working/StreamLight_daily_list_192sites.rds")
 
 # End of script.
